c3d1ebe4d7049ddbdf8393a8860e0c5d9f7dbd38
Your Name <you@example.com>
2023-05-08 20:46:42 -0700

    changed header color, following 60/30/10 rule

--- ocomni/src/components/Header.jsx
+++ ocomni/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Header() {
   return (
-    <div className="bg-[#ff0] border-b shadow-md sticky top-0 z-50 ">
+    <div className="shadow-md sticky top-0 z-50">
       <header className="flex flex-row justify-between items-center  mxa-w-6xl mx-auto h-10 px-10">
         <div>
           <img
@@ -13,10 +13,26 @@ export default function Header() {
         </div>
         <div>
           <ul className="flex space-x-8 ">
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-            <li>Sign In</li>
+            <li
+              className={`cursor-pointer font-semibold text-sm text-gray-400 border-b-3px border-b-transparent`}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer font-semibold text-sm text-gray-400 border-b-3px border-b-transparent`}
+            >
+              About
+            </li>
+            <li
+              className={`cursor-pointer font-semibold text-sm text-gray-400 border-b-3px border-b-transparent`}
+            >
+              Contact Us
+            </li>
+            <li
+              className={`cursor-pointer font-semibold text-sm text-gray-400 border-b-3px border-b-transparent`}
+            >
+              Sign In
+            </li>
           </ul>
         </div>
       </header>
