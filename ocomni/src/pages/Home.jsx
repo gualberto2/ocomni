a6672a3f55835e1e97b2d901fe989d76bb26de11
Arthur Fernandez <arthur@snooget.com>
2023-05-25 08:11:39 -0700

    added img and new conytainer to homepage

--- ocomni/src/pages/Home.jsx
+++ ocomni/src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+
 export default function HomePage() {
   return (
-    <div className="w-full  px-10 lg:px-28 lg:h-screen overflow-hidden">
+    <div className="min-h-screen min-w-screen px-10 lg:px-28 lg:h-screen overflow-hidden">
       <div className="lg:flex lg:pt-8">
         <div className="lg:w-[800px] container  xl:pt-24 lg:pt-10 pt-20">
           <h1 className=" font-extrabold font-[1000] xl:text-[4.2rem] lg:text-[3rem] md:text-[3.2rem] text-3xl lg:w-[79%] text-black xl:leading-[5rem] md:leading-[4rem]">
@@ -11,7 +12,7 @@ export default function HomePage() {
           <p className="text-transparent bg-clip-text bg-gradient-to-br from-[#000000] to-[#000000]">Professional grade E-Commerce Solutions</p>
         </div>
         <div className="xl:flex justify-start mt-7">
-          <div> {/* TRY TO ADD SVG IMG BELOW FOR RESPONSIVENESS */}
+          <div> {/* Call to action button that folds/wraps with mobile */}
             <button className="rounded px-7 py-3 bg-[#471FF7] text-white relative group hover:text-white overflow-hidden tracking-wider">
                 <span className="relative inset-0 flex justify-center items-center">
                   Work with us
@@ -20,15 +21,12 @@ export default function HomePage() {
           </div>
         </div>
       </div>
-      <div> {/* THIS IS WHERE CTA BUTTON GOES */}
-      <div className="xl:flex justify-start mt-7">
+
+      <div className="mx-auto w-full h-full"> {/* THIS IS WHERE SVG IMG GOES */}
+        <div className="">
           <div> {/* TRY TO ADD SVG IMG BELOW FOR RESPONSIVENESS */}
-            <button className="rounded px-7 py-3 bg-[#471FF7] text-white relative group hover:text-white overflow-hidden tracking-wider">
-                <span className="relative inset-0 flex justify-center items-center">
-                
-                </span>
-            </button>
-          </div>
+            <img src="" alt="home illistration" />
+          </div> 
         </div>
       </div>
     </div>
