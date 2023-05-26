6b7cda58443a0c56a00a678105ddfcf441b51cf2
Arthur Fernandez <arthur@snooget.com>
2023-05-25 18:42:31 -0700

    added side by side elements w/flex

--- ocomni/src/pages/Home.jsx
+++ ocomni/src/pages/Home.jsx
@@ -3,29 +3,27 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen min-w-screen px-10 lg:px-28 lg:h-screen overflow-hidden">
-      <div className="lg:flex lg:pt-8">
-        <div className="lg:w-[800px] container  xl:pt-24 lg:pt-10 pt-20">
-          <h1 className=" font-extrabold font-[1000] xl:text-[4.2rem] lg:text-[3rem] md:text-[3.2rem] text-3xl lg:w-[79%] text-black xl:leading-[5rem] md:leading-[4rem]">
-            E-Commerce Powered by <span className="text-transparent bg-clip-text bg-gradient-to-br from-[#471FF7] to-[#471FF7]">OCOMNI</span>
+    <div className="mx-8 md:flex lg:flex"> {/* This is the parent div needed for react the project looses spacing and padding without stuffhere */}
+        <div className="lg:w-[800px]"> {/* As of now this div affects the <h1> and  */}
+          <h1 className="font-[1000] xl:text-[4.2rem] lg:text-[3rem] md:text-[3.2rem] text-3xl font-extrabold">
+            E-Commerce Powered by <span className="text-purple">OCOMNI</span>
           </h1>
-          <p className="text-transparent bg-clip-text bg-gradient-to-br from-[#000000] to-[#000000]">Professional grade E-Commerce Solutions</p>
+          <p className="">Professional grade E-Commerce Solutions</p>
         </div>
-        <div className="xl:flex justify-start mt-7">
+        <div className="">
           <div> {/* Call to action button that folds/wraps with mobile */}
-            <button className="rounded px-7 py-3 bg-[#471FF7] text-white relative group hover:text-white overflow-hidden tracking-wider">
-                <span className="relative inset-0 flex justify-center items-center">
+            <button className="">
+                <span className="">
                   Work with us
                 </span>
             </button>
           </div>
         </div>
-      </div>
-
-      <div className="mx-auto w-full h-full"> {/* THIS IS WHERE SVG IMG GOES */}
+        <div className=""> {/* THIS IS WHERE SVG IMG GOES */}
+        {/* Below is the div for the png img, we want it sideways on lg screen and column on sm screen */}
         <div className="">
           <div> {/* TRY TO ADD SVG IMG BELOW FOR RESPONSIVENESS */}
-            <img src="" alt="home illistration" />
+            <img src="https://raw.githubusercontent.com/akibiaons/ocomni-art/main/ocomni/public/ocomni-home.png" alt="home illistration" />
           </div> 
         </div>
       </div>
--- ocomni/tailwind.config.js
+++ ocomni/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: '#5B45BB',
+      },
+    },
     fontFamily: {
       header: "Bold",
       primarybody: "LightSatoshi",
