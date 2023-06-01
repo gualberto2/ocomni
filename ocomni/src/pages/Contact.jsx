fedd5a76dc658de83989f16af2d4f890feb7ba57
gualberto2 <elijahwilsonnn@gmailcom>
2023-06-01 14:03:11 -0700

    fixed up the header to me more responsive. Starting contact page

--- ocomni/src/pages/Contact.jsx
+++ ocomni/src/pages/Contact.jsx
@@ -4,9 +4,23 @@ const ContactPage = () => {
   return (
     <div>
       <div>
-        <h1 className="text-3xl text-center font-semibold py-10 px-20 md:whitespace-nowrap">
+        <h1 className="text-3xl text-center font-medium py-10 px-20 md:whitespace-nowrap">
           We wanna hear from <em className="uppercase">You</em>.
         </h1>
+      </div>
+      <div className="grid grid-cols-1 grid-rows-1 sm: gap-6 mx-12 md:gap-0 md:justify-around">
+        <div className="">
+          <div className="border-2 border-black p-4">Box</div>
+        </div>
+        <div className="">
+          <div className="border-2 border-black p-4">Box</div>
+        </div>
+        <div className="">
+          <div className="border-2 border-black p-4">Box</div>
+        </div>
+        <div className="">
+          <div className="border-2 border-black p-4">Box</div>
+        </div>
       </div>
     </div>
   );
