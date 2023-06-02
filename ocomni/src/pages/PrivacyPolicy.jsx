8b8ce1fd0b8309236715ffbe0755dda9b6ee39ad
Arthur Fernandez <arthur@snooget.com>
2023-06-01 19:34:01 -0700

    added less than 50% of pp page

--- ocomni/src/pages/PrivacyPolicy.jsx
+++ ocomni/src/pages/PrivacyPolicy.jsx
@@ -3,8 +3,39 @@ import React from "react";
 const PrivacyPolicy = () => {
   return (
     <div>
-      <div className="">
-        PrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPagePrivacyPage
+      <div className="mx-auto container">
+        <h1 className="">Privacy Policy</h1>
+        <h3>Introduction</h3>
+        <p>
+          Ocomni.com (“We” or “Us” or “Our” or “Ocomni” or “Website”) is committed to ensure Your privacy while providing Our services (“Services”). If You want to know more about Our Services you need to provide certain necessary information through Our Website form. Submission of such personal information is voluntary and you may decline to answer Our questions at any time. However, by providing your personal information, You consent to do so for the purpose of assisting you in connection with Our Service.
+        </p>
+        <h3>What personal info is collected?</h3>
+        <p>
+          We collect name, email address, your project estimated budget and any other information you voluntarily submit to Us.    
+        </p>
+        <h3>How do we use your personal data</h3>
+        <p>
+          We may use personal information data in-house in order to provide and enhance the Service, to improve customers’ experience.
+        </p>
+        <h3>How and who gets to see your data</h3>
+        <p>
+          We use personal information collected via Our Website for a variety of business purposes described below. We process Your personal information for these purposes in reliance on Our legitimate business interests, in order to enter into or perform a contract with you, with your consent, and/or for compliance with Our legal obligations. We indicate the specific processing grounds We rely on next to each purpose listed below. We use the information We collect or receive:
+        </p>
+        <p>
+          We may use Your personal information to send you Service details, respond to your questions and provide other related information and/or information about changes to Our terms, conditions, and policies.
+        </p>
+        <p>
+          If We receive a subpoena or other legal request, We may need to inspect the data We hold to determine how to respond. We may disclose aggregate information about Our users for such legal purposes.
+        </p>
+        <p>
+          We may use Your information for other Business Purposes, such as data analysis, identifying usage trends, determining the effectiveness of Our Services, to evaluate and improve Our Services and Your experience. We will not use identifiable personal information without Your consent.
+        </p>
+        <h3>
+          Will Your information be shared with anyone
+        </h3>
+        <p>
+          When You provide the above-mentioned information to Us, You give Your consent that We may process or share data based on the following legal basis:
+        </p>
       </div>
     </div>
   );
