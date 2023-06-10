177ee6255f439e17f9a637e4f73b80a2a84df61b
Arthur Fernandez <arthur@snooget.com>
2023-06-10 15:48:07 -0700

    added fixed alignment for templates and bg clr

--- src/components/Templates.jsx
+++ src/components/Templates.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import template1 from "../assets/images/cowboy-template.png";
-import template2 from "../assets/images/eyeglass-template.png";
-import template3 from "../assets/images/plant-template.png";
+import template1 from "../assets/images/cowboy-2-template.png";
+import template2 from "../assets/images/reshoot-glasses.png";
+import template3 from "../assets/images/reshoot-flowers.png";
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination, Autoplay } from "swiper";
@@ -35,15 +35,15 @@ const Template = () => {
       <div className="text-center">
         <h2 className="mt-2 text-black text-4xl font-bold">Free Templates</h2>
         <p className="text-black mt-3 text-lg">
-          <span className="border-b-2 border-[#6366F1] border-hover:cursor-pointer hover:text-decoration-line:none mr-2">
+          <span className="border-b-2 border-[#6366F1] hover:border-b-2 hover:border-[#8c8de4] hover:text-decoration-line:none transition duration-150 mr-2 hover:cursor-pointer">
             Sign up
           </span>
           and get a FREE template
         </p>
       </div>
       <br />
-      <div className="flex max-w-6xl gap-6 px-5 mx-auto items-center relative">
-        <Swiper
+      <div className="flex justify-center mb-6"> {/*className="flex max-w-6xl gap-6 px-5 mx-auto items-center relative" I am trying to see which parent div has an affect on the layout of the cards and practice my flex / alignment practice.*/}
+        <Swiper /* Not completely sure if I can even add a classname and change the alignemt with the swiper components.*/
           slidesPreview={1.2}
           spaceBetween={20}
           breakpoints={{
@@ -62,13 +62,13 @@ const Template = () => {
         >
           {Templates.map((info, i) => (
             <SwiperSlide key={i}>
-              <div className="h-fit w-full p-4 bg-white rounded-xl">
+              <div className="flex flex-col items-center p-4 rounded-xl bg-slate-50"> {/*this is the parent div for img & h3 and seems to make them flex & flex col*/}
                 <img
                   src={info.img}
                   alt="downloadable template"
-                  className="rounded-lg"
+                  className="rounded-lg" /* This rounded class does not make much of a visual difference */
                 />
-                <h3 className="text-xl text-black uppercase font-medium text-center my-4">
+                <h3 className="text-md text-black uppercase font-medium text-center my-4">{/* This is the text that appears right under the image whih is the problem as it looks lopsided. */}
                   {info.title}
                 </h3>
               </div>
