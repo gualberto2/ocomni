04b587a34d0536af21b4ed29f74ab1d919a1d4df
Arthur Fernandez <arthur@snooget.com>
2023-06-11 14:27:11 -0700

    added the terms plugin

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import Iubenda from 'react-iubenda-policy';
 
-const PrivacyPolicy = () => {
+const Policy = () => {
+  const myPolicy = 56848444 // your policy id
   return (
     <div>
-      <div className="">Pri acy Page</div>
+      {/* Renders the Privacy Policy link with the text 'Privacy Policy' */}
+      <Iubenda id={myPolicy}/>
+        
+      <Iubenda id={myPolicy} type='terms-and-conditions' styling='nostyle'>
+        
+      </Iubenda>
+
+      <Iubenda id={myPolicy} type='privacy' styling='white'>
+
+      </Iubenda>
+ 
+      <Iubenda id={myPolicy} type='cookie' styling='black'>
+
+      </Iubenda>      
     </div>
-  );
+  )
 };
 
-export default PrivacyPolicy;
+export default Policy;
