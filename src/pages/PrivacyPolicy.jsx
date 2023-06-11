49082cf25f9b118b51319db7efa6d201a115e6b0
Arthur Fernandez <arthur@snooget.com>
2023-06-11 15:00:34 -0700

    added the framework for privacy policy

--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,26 +1,55 @@
 import React from "react";
-import Iubenda from 'react-iubenda-policy';
 
 const Policy = () => {
-  const myPolicy = 56848444 // your policy id
+  const myPolicy = 56848444; // your policy id
   return (
     <div>
-      {/* Renders the Privacy Policy link with the text 'Privacy Policy' */}
-      <Iubenda id={myPolicy}/>
-        
-      <Iubenda id={myPolicy} type='terms-and-conditions' styling='nostyle'>
-        
-      </Iubenda>
-
-      <Iubenda id={myPolicy} type='privacy' styling='white'>
-
-      </Iubenda>
- 
-      <Iubenda id={myPolicy} type='cookie' styling='black'>
-
-      </Iubenda>      
+      <div className="container mt-8 mx-auto border-b-2 border-slate-500">
+        <h1 className="text-xl font-bold">Ocomni Privacy Policy</h1>  
+        <p className="mt-4 mb-4">This application/website collects data from its users</p>  
+      </div>
+      <div className="container mx-auto mt-4">
+        <h2 className="font-bold">Personal Data processed for the following purposes and using the following services:</h2>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <div>
+              <h3>Analytics</h3>
+              <ul>
+                <li>
+                  <h3>Google Analytics</h3>
+                  <p>Personal Data: Trackers; Usage Data</p>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <div>
+              <h3>Connecting Data</h3>
+              <ul>
+                <li>
+                  <h3>Zapiar</h3>
+                  <p>Personal Data: Data communicated while using the service</p>
+                </li>
+              </ul>
+            </div>
+          </li>      
+          <li>
+            <div>
+              <h3>Managing contacts and sending messages</h3>
+              <ul>
+                <li>
+                  <h3>Mailchimp</h3>
+                  <p>Personal Data: Data communicated while using the service</p>
+                </li>
+              </ul>
+            </div>
+          </li>      
+        </ul>
+      </div>
     </div>
-  )
+  );
 };
 
 export default Policy;
