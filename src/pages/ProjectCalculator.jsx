9664cb030c649e25bdc309eb7d906ee79e872705
Arthur Fernandez <arthur@snooget.com>
2023-06-17 10:08:03 -0700

    First attempt at multi-form

--- src/pages/ProjectCalculator.jsx
+++ src/pages/ProjectCalculator.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import FormContext from "../components/form-components/SurveyContext";
+import Survey from "../components/Survey";
 
 export default function ProjectCalculator() {
   return (
-    <div>
-      <h1>Survey Page</h1>
-    </div>
+      <FormContext>
+        <Survey/>
+      </FormContext>
+    /* I will be importing the FormSurvey component, and the FormProvider components then nesting form in the
+    form provider component...*/
   );
 }
